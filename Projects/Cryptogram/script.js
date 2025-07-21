a4160a34a99fe16e984d9a79247157c3d3c4dc59
Rippey Fam <rippey.fam@gmail.com>
2025-07-21 11:59:02 -0600

    Added several features

--- Projects/Cryptogram/script.js
+++ Projects/Cryptogram/script.js
@@ -1,25 +1,21 @@
-const puzzles = ["Foo Bar Baz Testing Testing One Two Three Hello World", "HeLLO its a LemOncHeLLO"].sort((a, b) => Math.random() - 0.5)
+let puzzles = ["Foo Bar Baz Testing Testing One Two Three Hello World", "heLLO its a LemOncHeLLO"].sort((a, b) => Math.random() - 0.5)
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const randomAlphabet = [...alphabet].sort((a, b) => Math.random() - 0.5)
+let randomAlphabet;
 const alphabetMapping = new Array(alphabet.length)
-alphabetMapping.fill("-")
-console.log(alphabet)
-let currentPuzzle = puzzles.pop()
+let currentPuzzle;
+let deletedPuzzles = [];
 
 function findAlphabetNum(letter) {
     for (let j = 0; j < alphabet.length; j++) {
         if (letter.toUpperCase() === alphabet[j]) {
-            console.log("found letter " + alphabet[j] + " at position " + j)
             return j
         }
     }
-    console.log("couldn\'t find letter " + letter)
-    return "XYZ"
 }
 
 
 function generateCryptogram(code) {
-    const main = document.getElementsByTagName("main")[0]
+    let main = document.createElement("main")
     let sentence = document.createElement("div");
     sentence.setAttribute("class", "sentence");
     for (let i = 0; i < code.length; i++) {
@@ -42,14 +38,6 @@ function generateCryptogram(code) {
                     }
                 }
             })
-            letter.addEventListener("focus", (e) => {
-                let codes = document.querySelectorAll('div.box p:last-child');
-                for (let i = 0; i < codes.length; i++) {
-                    if (e.target.nextElementSibling.innerText === codes[i].innerText) {
-                        codes[i].previousElementSibling.classList.add("sameLetter")
-                    }
-                }
-            })
             letter.addEventListener("mouseleave", (e) => {
                 let codes = document.querySelectorAll('div.box p:first-child');
                 for (let i = 0; i < codes.length; i++) {
@@ -60,11 +48,9 @@ function generateCryptogram(code) {
             let letterToUse;
             if (alphabetMapping[alphabetPos] === "-") {
                 letterToUse = randomAlphabet.pop()
-                console.log("using new letter. chose " + letterToUse)
                 alphabetMapping[alphabetPos] = letterToUse
             } else {
                 letterToUse = alphabetMapping[alphabetPos]
-                console.log("using another " + letterToUse)
             }
             codeLetter.innerHTML = letterToUse
             if (code[i] === code[i].toUpperCase()) {
@@ -80,6 +66,7 @@ function generateCryptogram(code) {
         }
         main.append(sentence)
     }
+    document.body.appendChild(main);
 }
 function addListeners() {
     document.addEventListener('keypress', function (e) {
@@ -92,17 +79,19 @@ function addListeners() {
         }
         for (let i = 0; i < otherP.length; i++) {
             if (otherP[i] === selectedP) {
-                if (i != (otherP.length - 1))
-                    if (otherP[i].hasAttribute("tabindex")) {
-                        otherP[i + 1].focus();
+                if (i != (otherP.length - 1)) {
+                    if (otherP[i + 1].hasAttribute("tabindex")) {
+                        otherP[i + 1].focus()
+                        break
                     } else {
                         for (let j = i; j < otherP.length; j++) {
-                            if (otherP[i].hasAttribute("tabindex")) {
-                                otherP[i + 1].focus();
-                                break;
+                            if (otherP[j + 1].hasAttribute("tabindex")) {
+                                otherP[j + 1].focus()
+                                break
                             }
                         }
                     }
+                }
             }
         }
         let codes = document.querySelectorAll('div.box p:last-child');
@@ -111,12 +100,72 @@ function addListeners() {
                 codes[i].previousElementSibling.innerHTML = e.key.toUpperCase();
             }
         }
+    });
+    document.addEventListener("keydown", function (e) {
+        const selectedP = document.querySelector("div.box:focus");
+        const otherP = document.querySelectorAll("div.box");
+        if (e.key === "ArrowRight") {
+            for (let i = 0; i < otherP.length; i++) {
+                if (otherP[i] === selectedP) {
+                    if (i != (otherP.length - 1)) {
+                        if (otherP[i + 1].hasAttribute("tabindex")) {
+                            otherP[i + 1].focus()
+                            break
+                        } else {
+                            for (let j = i; j < otherP.length; j++) {
+                                if (otherP[j + 1].hasAttribute("tabindex")) {
+                                    otherP[j + 1].focus()
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (e.key === "ArrowLeft") {
+            for (let i = 0; i < otherP.length; i++) {
+                if (otherP[i] === selectedP) {
+                    if (i != 0) {
+                        if (otherP[i - 1].hasAttribute("tabindex")) {
+                            otherP[i - 1].focus()
+                            break
+                        } else {
+                            for (let j = i; j > 1; j--) {
+                                if (otherP[j - 1].hasAttribute("tabindex")) {
+                                    otherP[j - 1].focus()
+                                    break
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
+        if(e.key === "Escape"){
+            confirm("Make a new puzzle?")
+            newPuzzle()
+        }
     });
 }
 
+function newPuzzle() {
+    document.getElementsByTagName("main")[0].remove()
+    if (puzzles.length === 0) {
+        puzzles = [...deletedPuzzles]
+        deletedPuzzles = []
+    }
+    currentPuzzle = puzzles.pop()
+    console.log(currentPuzzle)
+    deletedPuzzles.unshift(currentPuzzle)
+    randomAlphabet = [...alphabet].sort((a, b) => Math.random() - 0.5)
+    console.log("random alphabet: " + randomAlphabet)
+    alphabetMapping.fill("-")
+    generateCryptogram(currentPuzzle)
+}
 
 document.addEventListener("DOMContentLoaded", e => {
-    generateCryptogram(currentPuzzle)
+    newPuzzle()
     addListeners()
 })
