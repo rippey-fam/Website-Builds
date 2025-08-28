4ddd30ae03a8eabae8155d1ab169bb6530f85870
Rippey Fam <rippey.fam@gmail.com>
2025-08-28 14:46:46 -0600

    Created more settings for Gravity

--- Projects/Gravity/main.js
+++ Projects/Gravity/main.js
@@ -5,8 +5,17 @@ const fullscreenButton = document.querySelector("#fullscreen-button")
 canvas.width = wrapper.clientWidth - 20
 canvas.height = wrapper.clientHeight - 25
 
+function clamp(variable, min, max) {
+    if (variable > max) return max;
+    if (variable < min) return min;
+    return variable
+}
+
 let friction = 0;
 let tailLen = 100
+let mouseGravity = true;
+let ballGravity = false;
+let bouncy = false
 class Dot {
     colorsCount = 12
     constructor(x, y) {
@@ -20,48 +29,50 @@ class Dot {
     }
     setFriction(newFriction) {
         friction = newFriction
+        if (newFriction !== 0) {
+            friction += ((Math.random() - 0.5) * 2) * 0.02
+        }
     }
     setTailLen(len) {
         tailLen = len
     }
     act() {
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let dist = Math.sqrt(dx * dx + dy * dy);
-        if (dist !== 0) {
-            dx /= dist;
-            dy /= dist;
-            this.velocity.x += dx * this.accelerationSpeed;
-            this.velocity.y += dy * this.accelerationSpeed;
-
-        }
+        let bodies = []
+        if(mouseGravity) bodies.push(mouse);
+        if(ballGravity) bodies.push(...dots)
+        bodies.forEach((body) => {
+            if (body !== this) {
+                let dx = body.x - this.x;
+                let dy = body.y - this.y;
+                let dist = Math.sqrt(dx * dx + dy * dy);
+                if (dist !== 0) {
+                    dx /= dist;
+                    dy /= dist;
+                    this.velocity.x += dx * this.accelerationSpeed;
+                    this.velocity.y += dy * this.accelerationSpeed;
+                }
+            }
+        })
         this.velocity.x -= friction * Math.sign(this.velocity.x)
         this.velocity.y -= friction * Math.sign(this.velocity.y)
     }
     move() {
         this.x += this.velocity.x
         this.y += this.velocity.y
-        // Keep the dot inside the canvas bounds and stop velocity if it hits the edge
-        if (this.x > canvas.width - this.radius) {
-            this.x = canvas.width - this.radius;
-            this.velocity.x = 0;
+        if (this.x > canvas.width - this.radius || this.x < this.radius) {
+            this.velocity.x = bouncy ? -this.velocity.x : 0;
+            if(this.x < this.radius) this.x = this.radius
+            else this.x = canvas.width - this.radius
         }
-        if (this.x < this.radius) {
-            this.x = this.radius;
-            this.velocity.x = 0;
-        }
-        if (this.y > canvas.height - this.radius) {
-            this.y = canvas.height - this.radius;
-            this.velocity.y = 0;
-        }
-        if (this.y < this.radius) {
-            this.y = this.radius;
-            this.velocity.y = 0;
+        if (this.y > canvas.height - this.radius || this.y < this.radius) {
+            this.velocity.y = bouncy ? -this.velocity.y: 0;
+            if(this.y < this.radius) this.y = this.radius;
+            else this.y = canvas.height - this.radius;
         }
         this.points.push({ x: this.x, y: this.y })
-        if (this.points.length > tailLen) {
-            while(this.points.length > tailLen){this.points.shift()}
-            
+        if (this.points.length >= tailLen) {
+            while (this.points.length > tailLen + 1) this.points.shift();
+
         }
     }
     draw() {
@@ -117,10 +128,11 @@ document.addEventListener("DOMContentLoaded", () => {
     game()
 })
 
-let mouse = { x: 0, y: 0 }
+let mouse = { x: canvas.width / 2, y: canvas.height / 2 }
 document.addEventListener("mousemove", (e) => {
-    mouse.x = e.offsetX;
-    mouse.y = e.offsetY;
+    let canvasBounds = canvas.getBoundingClientRect()
+    mouse.x = e.x - canvasBounds.x;
+    mouse.y = e.y - canvasBounds.y;
 });
 
 canvas.addEventListener("click", (e) => {
@@ -129,7 +141,7 @@ canvas.addEventListener("click", (e) => {
 
 canvas.addEventListener("dblclick", (e) => {
     for (let i = 0; i < 5; i++) {
-        dots.push(new Dot(e.offsetX + (Math.random() - 0.5) * 1000, e.offsetY + (Math.random() - 0.5) * 50))
+        dots.push(new Dot(e.offsetX + (Math.random() - 0.5) * 500, e.offsetY + (Math.random() - 0.5) * 500))
     }
 })
 
@@ -137,29 +149,81 @@ let displayButton = true;
 fullscreenButton.addEventListener("click", (e) => {
     wrapper.requestFullscreen()
     fullscreenButton.style.display = "none"
-    console.log("fullScreen")
+    deleteButton.style.display = "none"
 })
 
 wrapper.addEventListener("fullscreenchange", (e) => {
     if (!displayButton) {
         fullscreenButton.style.display = "flex"
+        deleteButton.style.display = "flex"
         displayButton = true
     } else {
         displayButton = false
     }
-    console.log("fullScreen change")
 })
 
 const frictionEl = document.querySelector("#friction")
 const frictionDisp = document.querySelector("#friction-display")
-frictionEl.addEventListener("input", (e)=>{
+frictionEl.addEventListener("input", () => {
     friction = frictionEl.value
     frictionDisp.innerHTML = frictionEl.value
 })
 
 const tailLenEl = document.querySelector("#tail-length")
 const tailLenDisp = document.querySelector("#tail-length-display")
-tailLenEl.addEventListener("input", (e) => {
+tailLenEl.addEventListener("input", () => {
     tailLen = tailLenEl.value
     tailLenDisp.innerHTML = tailLenEl.value
+})
+
+const mouseGravityEl = document.querySelector("#mouse-gravity")
+mouseGravityEl.addEventListener("input", ()=>{
+    mouseGravity = mouseGravityEl.checked
+})
+
+const ballGravityEl = document.querySelector("#ball-gravity")
+ballGravityEl.addEventListener("input", ()=>{
+    ballGravity = ballGravityEl.checked 
+})
+
+const bouncyEl = document.querySelector("#bouncy")
+bouncyEl.addEventListener("input", ()=>{
+    bouncy = bouncyEl.checked
+})
+
+const deleteButton = document.querySelector("#trash-button")
+deleteButton.addEventListener("click", ()=>{
+    dots = []
+})
+
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "ArrowLeft":
+            tailLen -= 5
+            tailLen = clamp(tailLen, 0, 1000)
+            tailLenEl.value = tailLen
+            tailLenDisp.innerHTML = tailLenEl.value
+            break
+        case "ArrowRight":
+            tailLen += 5
+            tailLen = clamp(tailLen, 0, 1000)
+            tailLenEl.value = tailLen
+            tailLenDisp.innerHTML = tailLenEl.value
+            break
+        case "m":
+            mouseGravityEl.checked = !mouseGravityEl.checked
+            mouseGravity = mouseGravityEl.checked
+            break
+        case "b":
+            ballGravityEl.checked = !ballGravityEl.checked
+            ballGravity = ballGravityEl.checked
+            break
+        case "o":
+            bouncyEl.checked = !bouncyEl.checked
+            bouncy = bouncyEl.checked
+            break
+        default:
+            break
+
+    }
 })
