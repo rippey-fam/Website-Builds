2f295b136744f66a45dce1f26c46a2d52d75aa96
Rippey Fam <rippey.fam@gmail.com>
2025-11-07 14:04:47 -0700

    More CSS

--- src/Computer Club/Back Attack/POC/main.js
+++ src/Computer Club/Back Attack/POC/main.js
@@ -1,6 +1,13 @@
-import "./firebase/firestore.js";
+import { data } from "./firebase/firestore.js";
 
 document.querySelector("#m-button").addEventListener("click", () => {
     document.querySelector("#group-chat").innerHTML += "<p>M: " + document.querySelector("#m-textarea").value + "<p>";
+    data.setMessage({ name: "Matthew", message: "M: " + document.querySelector("#m-textarea").value });
+    document.querySelector("#m-textarea").value = "";
+});
+
+document.querySelector("#s-button").addEventListener("click", () => {
+    document.querySelector("#group-chat").innerHTML += "<p>S: " + document.querySelector("#m-textarea").value + "<p>";
+    data.setMessage({ name: "Scott", message: "S: " + document.querySelector("#m-textarea").value });
     document.querySelector("#m-textarea").value = "";
 });
