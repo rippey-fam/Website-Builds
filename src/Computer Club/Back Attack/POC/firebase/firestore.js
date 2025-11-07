5aa9b817e68655f409c15819e7513f19a1c46b12
Scott Rippey <scott.william.rippey@gmail.com>
2025-11-07 13:32:08 -0700

    feat(firestore): added firestore config

---
+++ src/Computer Club/Back Attack/POC/firebase/firestore.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKmPbS-dplczSxkRkAJ4lg9vZf3467DTg",
+    authDomain: "back-attack-poc.firebaseapp.com",
+    projectId: "back-attack-poc",
+    storageBucket: "back-attack-poc.firebasestorage.app",
+    messagingSenderId: "415017495411",
+    appId: "1:415017495411:web:f0e899582d762520c2b306",
+    measurementId: "G-HT0ECVG2HJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
