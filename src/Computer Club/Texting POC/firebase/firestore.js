c24abe0fcea39969fba8af8cc8310f6a0828c520
Matthew <rippey.fam@gmail.com>
2026-01-04 17:07:18 -0700

    Update firestore.js

--- src/Computer Club/Texting POC/firebase/firestore.js
+++ src/Computer Club/Texting POC/firebase/firestore.js
@@ -7,13 +7,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDKmPbS-dplczSxkRkAJ4lg9vZf3467DTg",
-    authDomain: "back-attack-poc.firebaseapp.com",
-    projectId: "back-attack-poc",
-    storageBucket: "back-attack-poc.firebasestorage.app",
-    messagingSenderId: "415017495411",
-    appId: "1:415017495411:web:f0e899582d762520c2b306",
-    measurementId: "G-HT0ECVG2HJ",
+  apiKey: "AIzaSyDKmPbS-dplczSxkRkAJ4lg9vZf3467DTg",
+  authDomain: "back-attack-poc.firebaseapp.com",
+  projectId: "back-attack-poc",
+  storageBucket: "back-attack-poc.firebasestorage.app",
+  messagingSenderId: "415017495411",
+  appId: "1:415017495411:web:f0e899582d762520c2b306",
+  measurementId: "G-HT0ECVG2HJ"
 };
 
 // Initialize Firebase
