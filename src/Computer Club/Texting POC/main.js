3b82d6bfbfa6bf33ea87099afc882b559c056912
Rippey Fam <rippey.fam@gmail.com>
2026-01-04 17:57:56 -0700

    Working on my POC again!

--- src/Computer Club/Texting POC/main.js
+++ src/Computer Club/Texting POC/main.js
@@ -1,22 +1,26 @@
 import { data } from "./firebase/firestore.js";
 
 let id = 0;
-
+let name;
+// if it's not working, it might be because you need to change the rules at https://console.firebase.google.com/project/back-attack-poc/firestore/databases/-default-/security/rules
 data.onMessagesUpdate((messages) => {
     document.querySelector("#group-chat").innerHTML = "";
     messages = messages.sort((a, b) => a.id - b.id);
     messages.forEach((message) => {
         if (message.id > id) id = message.id;
-        document.querySelector("#group-chat").innerHTML += "<p>" + message.message + "<p>";
+        if (message.message.trim() !== "")
+            document.querySelector("#group-chat").innerHTML += "<p>" + message.message + "<p>";
     });
 });
 
-document.querySelector("#m-button").addEventListener("click", () => {
-    data.setMessage({ id: ++id, message: "M: " + document.querySelector("#m-textarea").value });
-    document.querySelector("#m-textarea").value = "";
+document.querySelector("#send-button").addEventListener("click", () => {
+    if (document.querySelector("#send-textarea").value.trim() !== "") {
+        data.setMessage({ id: ++id, message: `${name}: ` + document.querySelector("#send-textarea").value.trim() });
+        document.querySelector("#send-textarea").value = "";
+    }
 });
 
-document.querySelector("#s-button").addEventListener("click", () => {
-    data.setMessage({ id: ++id, message: "S: " + document.querySelector("#m-textarea").value });
-    document.querySelector("#m-textarea").value = "";
+document.querySelector("#popup > div > button").addEventListener("click", () => {
+    name = document.querySelector("#popup > div > textarea").value.trim() || "Anonymous";
+    document.querySelector("#popup").style.display = "none";
 });
