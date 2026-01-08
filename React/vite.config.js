5ed0bbffb963b2b6cac36554d12546d6d08b2e40
Rippey Fam <rippey.fam@gmail.com>
2026-01-07 17:15:14 -0700

    Started work on a React Website. lots of errors so far

--- React/vite.config.js
+++ React/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { dirname, resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    root: resolve(__dirname, "./src/Projects"),
+    build: {
+        rollupOptions: {
+            input: {
+                tenThings: resolve(__dirname, "./src/Projects/ThereAreTenPeople/index.html"),
+                weatherApp: resolve(__dirname, "./src/Projects/WeatherApp/index.html"),
+            },
+        },
+    },
+});
